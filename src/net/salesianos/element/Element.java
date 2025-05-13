932bbb69bd0230fc70951e562ba777988b06e999
oscarrdelgadooy <usr3638@salesianos-lacuesta.net>
2025-05-13 09:46:54 +0100

    Creación de la rama element y de la clase

--- src/net/salesianos/element/Element.java
+++ src/net/salesianos/element/Element.java
@@ -1,5 +1,17 @@
 package net.salesianos.element;
 
 public class Element {
+    private String name;
+    private int ranking;
+    private float price;
+    private int numberCigarretes;
+
+    public Element(String name, int ranking, float price, int sizes) {
+        this.name = name;
+        this.ranking = ranking;
+        this.price = price;
+        this.numberCigarretes = sizes;
+    }
+
     
 }
