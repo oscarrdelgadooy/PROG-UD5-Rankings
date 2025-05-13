3de7d850be4d4a41493d883803482d73f10fa011
oscarrdelgadooy <usr3638@salesianos-lacuesta.net>
2025-05-13 10:15:50 +0100

    Docs(Tobacco): Cambio del fichero element a Tobacco

--- src/net/salesianos/element/Tobacco.java
+++ src/net/salesianos/element/Tobacco.java
@@ -1,12 +1,12 @@
 package net.salesianos.element;
 
-public class Element {
+public class Tobacco {
     private String name;
     private int ranking;
     private float price;
     private int numberCigarretes;
 
-    public Element(String name, int ranking, float price, int sizes) {
+    public Tobacco(String name, int ranking, float price, int sizes) {
         this.name = name;
         this.ranking = ranking;
         this.price = price;
