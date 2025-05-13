587ef2d5cb1c9f1a4c6d777c6499aa7100e49a02
oscarrdelgadooy <usr3638@salesianos-lacuesta.net>
2025-05-13 09:52:37 +0100

    Getters, Setters y Override toString

--- src/net/salesianos/element/Element.java
+++ src/net/salesianos/element/Element.java
@@ -13,5 +13,46 @@ public class Element {
         this.numberCigarretes = sizes;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getNumberCigarretes() {
+        return numberCigarretes;
+    }
+
+    public void setNumberCigarretes(int numberCigarretes) {
+        this.numberCigarretes = numberCigarretes;
+    }
+
+    @Override
+    public String toString(){
+        String textToConsole = "";
+        textToConsole += "---------------------------------------\n";
+        textToConsole += "- Nombre:" + name;
+        textToConsole += "\n- Ranking: " + ranking;
+        textToConsole += "\n- Precio: " + price;
+        textToConsole += "\n- "
+    }
     
 }
