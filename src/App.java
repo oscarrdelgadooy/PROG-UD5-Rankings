c9153df02c39698e875278e7eda869799e7ddbde
oscarrdelgadooy <usr3638@salesianos-lacuesta.net>
2025-05-13 10:58:45 +0100

    feat(App.java): Creación de array de marcas de tabaco.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import javax.swing.JOptionPane;
+
+import net.salesianos.element.Tobacco;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {        
+        final Tobacco[] typesTobaccos = {
+            new Tobacco("Marlboro", 4, 5.2f, 20),
+            new Tobacco("Lm", 4, 3.8f, 20),
+            new Tobacco("Paramount", 1, 2.3f, 20),
+            new Tobacco("Benson&Hedges", 0, 0, 0)
+        };
+
+        int ask = JOptionPane.showConfirmDialog(null, "¿Quieres continuar?");
+        do {
+
+        } while (JOptionPane.OK_OPTION == ask);
     }
 }
