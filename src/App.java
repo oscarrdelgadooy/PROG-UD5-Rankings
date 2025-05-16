d7ff66e5d3145515777b7bedbd9101902b72f534
oscarrdelgadooy <oscardelyanes@gmail.com>
2025-05-16 11:48:04 +0100

    feat(App.java): Creacion de la lista de la clase ejecutable

--- src/App.java
+++ src/App.java
@@ -8,7 +8,10 @@ public class App {
             new Tobacco("Marlboro", 4, 5.2f, 20),
             new Tobacco("Lm", 4, 3.8f, 20),
             new Tobacco("Paramount", 1, 2.3f, 20),
-            new Tobacco("Benson&Hedges", 0, 0, 0)
+            new Tobacco("Benson&Hedges 100s", 3, 3.2f, 20),
+            new Tobacco("Chesterfield 24s", 3, 3.8f, 24),
+            new Tobacco("Kruger", 0, 3f, 20),
+            new Tobacco("Corona", 2, 3.5f, 20)
         };
 
         int ask = JOptionPane.showConfirmDialog(null, "¿Quieres continuar?");
