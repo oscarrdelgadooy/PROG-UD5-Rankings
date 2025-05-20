c96f703c1845765330292ec40a68b65b856e4e32
oscarrdelgadooy <oscardelyanes@gmail.com>
2025-05-20 12:50:40 +0100

    feat(Validation.java): Añadir algunas validaciones(SIN IMPLEMENTACION)

--- src/net/salesianos/utils/Validation.java
+++ src/net/salesianos/utils/Validation.java
@@ -1,5 +1,8 @@
 package net.salesianos.utils;
 
 public interface Validation {
-    
+    boolean validateName(String nombre);
+    boolean validateRanking(String rankingTexto);
+    boolean validatePrice(String precioTexto);
+    boolean validateSize(String tamanioTexto);
 }
